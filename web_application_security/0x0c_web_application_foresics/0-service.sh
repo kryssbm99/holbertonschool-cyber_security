5444c7f8c19ea76321028a360b0dc62d958368ac
Kryss <kryssbm99@gmail.com>
2025-04-07 18:22:09 -0500

    Task #0 Web App Forensics

---
+++ web_application_security/0x0c_web_application_foresics/0-service.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+LOG_FILE="auth.log"
+
+if [ ! -f "$LOG_FILE" ]; then
+    echo "Log file not found!"
+    exit 1
+fi
+
+grep "pam_unix" "$LOG_FILE" | \
+    grep -oP 'pam_unix\(\K[^:]+' | \
+    sort | uniq -c | sort -nr | \
+    head -n 1 | awk '{print $2}'
