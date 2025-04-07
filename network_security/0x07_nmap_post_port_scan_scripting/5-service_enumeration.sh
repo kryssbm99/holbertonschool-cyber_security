0abcc60cab58c79e45a6e499bdde569ab9053a78
Kryss <kryssbm99@gmail.com>
2025-04-07 18:03:38 -0500

    Task #5 Nmap Scan

--- network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nmap -sS -sV -O --script banner,ssl-enum-ciphers,default,smb-enum-domains --traceroute $1 -oN service_enumeration_results.txt
+nmap -A --script banner,ssl-enum-ciphers,default,smb-enum-domains $1 -oN service_enumeration_results.txt
